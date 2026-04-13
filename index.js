539d768cabca09b1233b108cfb285ff040bf19f3
Ubuntu <ubuntu@ip-172-31-64-231.ec2.internal>
2026-04-13 10:00:02 +0000

    Fix app listen for test and deploy

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ app.get('/', function (req, res) {
   res.send('CI/CD pipeline working successfully!');
 });
 
-app.listen(3000, '0.0.0.0', function () {
-  console.log('App is running on port 3000');
-});
+if (require.main === module) {
+  app.listen(3000, '0.0.0.0', function () {
+    console.log('App is running on port 3000');
+  });
+}
 
 module.exports = app;
